992f7bcd1b14da3004096a50b08f26fb407b7df8
Paschalia Nsato <paschaliansato@Paschalias-MacBook-Pro.local>
2018-04-27 18:27:49 -0400

    modified sketch added interactivity

--- sketch.js
+++ sketch.js
@@ -6,7 +6,12 @@ function draw(){
 //canvasbackground
 background(88,185,227);
 point(500,400);
+
+push();
+translate(0, mouseY);
+
 //vynel
+beginShape();
 //black ellipse
 strokeWeight(5);
 stroke(72,72,72);
@@ -20,20 +25,30 @@ ellipse(520,300,150,150);
 noStroke();
 
 //strokes
+
 strokeWeight(1);
 noFill();
 stroke(255, 255, 255, 255)
  ellipse(525, 300, 350, 350, 0)
  ellipse(525, 300, 300, 300, 0)
  ellipse(525, 300, 200, 200, 0)
+ endShape(CLOSE);
+ pop();
 
 
 
 //rectangle 
+if(mouseY>200){
 stroke(0, 0, 0,);
 strokeWeight(10);
 fill(39,5,55);
 rect(300,300, 450, 350);
+
+textSize(30);
+noStroke();
+fill(255);
+text("Money Mondays", 400, 400);
+}
 
 
 // //triangle
